8e0768a2db9c59e92315d176b037b91707e59fe3
rayshareifikar <nyananyana23@gmail.com>
2024-11-13 14:18:29 +0700

    Tutorial 8 DONE

--- lib/screens/menu.dart
+++ lib/screens/menu.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:mental_health_tracker/widgets/left_drawer.dart';
 import 'package:mental_health_tracker/widgets/mood_card.dart';
+import 'package:mental_health_tracker/screens/moodentry_form.dart';
+
 
 class MyHomePage extends StatelessWidget {
   MyHomePage({super.key});
